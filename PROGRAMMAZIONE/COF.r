eb9aa45060927fa59a5dd86a145fa45a84e209a9
Massimo Tranquillo <tmax@Air-di-Massimo.lan>
2020-09-20 22:07:36 +0200

    update

--- PROGRAMMAZIONE/COF.r
+++ PROGRAMMAZIONE/COF.r
@@ -1,20 +1,36 @@
 library(readxl)
 library(tidyverse)
-bg <- read_excel("D:/Dati/vito.tranquillo/Desktop/GitProjects/IZSLER-COGES/PROGRAMMAZIONE/bg.xlsx")
-anag <- read_excel("D:/Dati/vito.tranquillo/Desktop/GitProjects/IZSLER-COGES/PROGRAMMAZIONE/anagrafe.xlsx")
+
+#bg <- read_excel("D:/Dati/vito.tranquillo/Desktop/GitProjects/IZSLER-COGES/PROGRAMMAZIONE/bg.xlsx")
+#anag <- read_excel("D:/Dati/vito.tranquillo/Desktop/GitProjects/IZSLER-COGES/PROGRAMMAZIONE/anagrafe.xlsx")
 time <- read_excel("D:/Dati/vito.tranquillo/Desktop/GitProjects/IZSLER-COGES/PROGRAMMAZIONE/presenze.xlsx")
 
-
+setwd("~/Library/Mobile Documents/com~apple~CloudDocs/gitProject/IZSLER-COGES/PROGRAMMAZIONE")
+bg <- read_excel("bg.xlsx")
+anag <- read_excel("anagrafe.xlsx")
+time <- read_excel("presenze.xlsx")
 
 #FULL TIME EQUIVALENT####
 View(time)
 
+time$hr <- time$Minuti/60
 
- 
+time %>% 
+  group_by(Mese) %>% 
+  summarise(hrm=sum(hr)) %>% 
+  mutate(fte = hrm / 1449) %>% 
+  
 #Attività####
-bg <- bg %>% 
-  filter(settore !="Controlli Interni Sistema Qualità")
+bg %>% 
+  group_by(settore) %>% 
+  summarise(esami=sum(esami, na.rm = T)) %>% 
+  arrange(desc(esami)) %>% 
+  janitor::adorn_totals(where = "row")
+  View()
+
+  
+
+
 
 attività <- unique(factor(bg$FinalitàConf))
 
-filtro <- c("Progetto: PRC2018005", "Circuiti interni di laboratorio", "Qualifica/mantenimento qualifica personale")
