baa5871d00c08a85adb1ebdec7a6d33b0c38b6f5
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2020-09-22 15:07:11 +0200

    update

--- PROGRAMMAZIONE/COF.r
+++ PROGRAMMAZIONE/COF.r
@@ -1,14 +1,15 @@
 library(readxl)
 library(tidyverse)
+library(lubridate)
 
-#bg <- read_excel("D:/Dati/vito.tranquillo/Desktop/GitProjects/IZSLER-COGES/PROGRAMMAZIONE/bg.xlsx")
-#anag <- read_excel("D:/Dati/vito.tranquillo/Desktop/GitProjects/IZSLER-COGES/PROGRAMMAZIONE/anagrafe.xlsx")
+bg <- read_excel("D:/Dati/vito.tranquillo/Desktop/GitProjects/IZSLER-COGES/PROGRAMMAZIONE/bg.xlsx")
+anag <- read_excel("D:/Dati/vito.tranquillo/Desktop/GitProjects/IZSLER-COGES/PROGRAMMAZIONE/anagrafe.xlsx")
 time <- read_excel("D:/Dati/vito.tranquillo/Desktop/GitProjects/IZSLER-COGES/PROGRAMMAZIONE/presenze.xlsx")
 
-setwd("~/Library/Mobile Documents/com~apple~CloudDocs/gitProject/IZSLER-COGES/PROGRAMMAZIONE")
-bg <- read_excel("bg.xlsx")
-anag <- read_excel("anagrafe.xlsx")
-time <- read_excel("presenze.xlsx")
+# setwd("~/Library/Mobile Documents/com~apple~CloudDocs/gitProject/IZSLER-COGES/PROGRAMMAZIONE")
+# bg <- read_excel("bg.xlsx")
+# anag <- read_excel("anagrafe.xlsx")
+# time <- read_excel("presenze.xlsx")
 
 #FULL TIME EQUIVALENT####
 View(time)
@@ -16,24 +17,44 @@ View(time)
 time$hr <- time$Minuti/60
 
 time %>% 
-  group_by(Mese, Matricola) %>% 
+  group_by(Matricola) %>% 
   summarise(hrm=sum(hr)) %>% 
   left_join(anag, by="Matricola") %>% 
   drop_na() %>% 
-  group_by(Categoria,Matricola) %>% 
+  group_by(Categoria,Matricola, Cognome) %>% 
   summarise(hrm = sum(hrm))
 
 
   
 #Attività####
+
+tabella <- bg %>% 
+  mutate(mese = month(datareg)) %>% 
+  group_by(settore, prova, tecnica, labs) %>% 
+  summarise(Esami = sum(esami, na.rm = T)) %>% 
+  #select(settore, prova, esami) %>% 
+  unique() 
+
+write.csv(tabella, file = "tabella.csv")
+
 bg %>% 
   group_by(settore) %>% 
   summarise(esami=sum(esami, na.rm = T)) %>% 
   arrange(desc(esami)) %>% 
   janitor::adorn_totals(where = "row")
 
-  
+bg %>% 
+  mutate(mese = month(datareg)) %>% 
+  group_by(labs) %>% 
+  summarise(Esami = sum(esami, na.rm = T)) %>% 
+  janitor::adorn_totals(where = "row") %>% 
+  #select(settore, prova, esami) %>% 
+  unique() 
 
+
+
+bg %>% 
+  
 
 
 attività <- unique(factor(bg$FinalitàConf))
