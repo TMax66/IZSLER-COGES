d4de35bb723f1b7fd73a890d414fff36fd1d2e30
Massimo Tranquillo <tmax@Air-di-Massimo.lan>
2020-11-23 21:15:45 +0100

    update

--- PROGRAMMAZIONE/shinyapp/COF.R
+++ PROGRAMMAZIONE/shinyapp/COF.R
@@ -1,26 +1,10 @@
-library("tidyverse")
-library("networkD3")
-library("hrbrthemes")
-library("readxl")
-library("ggrepel")
-library("RColorBrewer")
-library("wesanderson")
-library("DT")
-library("shiny")
-library("shinydashboard")
-library("here")
-library("knitr")
-library("kableExtra")
-library("formattable")
-library("shinythemes")
-library("rpivotTable")
-library("here")
-options(scipen = .999)
 dati <- readRDS( here("programmazione", "shinyapp", "dati.rds"))
 dati <- dati %>% 
   mutate(across(where(is.numeric), function(x) round(x, 2)))
 vp <- readRDS( here("programmazione", "shinyapp", "vp.rds"))
 ai <- readRDS( here("programmazione", "shinyapp", "ai.rds"))
+
+
 
 dir <- dati %>%
   filter(contratto == "DIRIGENZA") %>%
@@ -29,12 +13,14 @@ dir <- dati %>%
             ricavi = sum(ricavi),
             FTE_d = round(sum(`FTE-reale`),1))
 
+
 comp <- dati %>%
   filter(contratto == "COMPARTO") %>%
   group_by(Dipartimento) %>%
   summarise(esami = sum(esami),
             ricavi = sum(ricavi),
             FTE_c = round(sum(`FTE-reale`),1))
+
 vp <- vp %>% 
   group_by(Dipartimento) %>% 
   summarise(VP = sum(`Vendita Prodotti`))
@@ -43,17 +29,20 @@ ai <- ai %>%
   group_by(Dipartimento) %>% 
   summarise(AI = sum(`Attività Interna`))
 
+
+
 tabella <- dir %>%
   bind_cols((comp %>%
                select(4)), (vp %>%
                               select(2)), (ai %>%
                                              select(2))) %>%
+
   mutate(RT = (ricavi+VP+AI),
          FTE_t = round((FTE_d+FTE_c),1)) %>%
   arrange(desc(esami)) %>%
   adorn_totals(where = "row") %>%
-  mutate("R-FTE" = round(RT/FTE_t,0) ) %>%
+  mutate( "R-FTE" = round(RT/FTE_t,0) ) %>%
   select(Dipartimento, "N.esami" = esami, "FTED" = FTE_d,   "FTEC" = FTE_c, "FTET" = FTE_t, "RA" = ricavi, "RVP" = VP,
          "RAI" = AI, "RT" = RT, "R/FTET" = "R-FTE")
-
-
+ 
+tabella
--- PROGRAMMAZIONE/shinyapp/server.R
+++ PROGRAMMAZIONE/shinyapp/server.R
@@ -1,249 +1,32 @@
-server<-function(input, output) { 
-
-#### Dipartimento ##### 
-  dt <-reactive(
-    dati %>% ungroup() %>% 
-      group_by(Dipartimento) %>% 
-      summarise(hworked = sum(hworked)/1000, 
-                hprev = sum(hprev)/1000, 
-                esami = sum(esami)/1000, 
-                ricavi = sum (ricavi)/1000) %>% 
-      mutate("FTEpr" = (1000*hprev)/(36*45.6), 
-             "FTEr" = (1000*hworked)/(36*45.6),
-             "Perchwd" = 100*(hworked/hprev),
-             "tempo medio esame" = 60*(hworked/esami)) %>% 
-      pivot_longer(2:9, names_to = "indicatore", values_to ="valore") %>%  
-      filter(indicatore==input$indicatore))
+server <- function(input, output, session) { 
   
+###Quadro Generale Dashboard#####
   
-  output$diplot <-renderPlot({
-    dt() %>%
-      arrange(valore) %>%
-      mutate(Dipartimento = factor(Dipartimento, levels = .$Dipartimento)) %>%
-      mutate(media = mean(valore, na.rm = TRUE)) %>%  
-      
-      ggplot(aes(x=Dipartimento, y=valore, label = round(valore, 1)))+
-      geom_point( aes(x=Dipartimento, y=valore),color="lightblue", size = 15 )+
-      geom_text(color = "black", size = 5)+
-      geom_segment(aes(y = media, x = Dipartimento, yend = valore, xend = Dipartimento ),color = "grey50") +
-      coord_flip() +
-      theme_ipsum_rc(axis_title_just = "mc")+
-      labs(y=input$indicatore,x="")+
-      theme(axis.text.x = element_text(color="blue", size=12, face="bold"))+
-      theme(axis.text.y = element_text(color="blue", size=12, face="bold"))+
-      theme(axis.title.x = element_text(size = 12, face = "bold"))
+output$esami <- renderValueBox({
+    valueBox(
+      tabella %>% 
+        filter(Dipartimento == "Total") %>% 
+        select("N.esami"), "# esami", icon = icon("keyboard"),
+      color = "navy"
+    )
   })
   
-  dtx <-reactive(dati %>% ungroup() %>% 
-                   group_by(Dipartimento) %>% 
-                   summarise(hworked = sum(hworked), 
-                             hprev = sum(hprev), 
-                             esami = sum(esami), 
-                             ricavi = sum (ricavi)) %>% 
-                   mutate("FTE-previsto" = hprev/(36*45.6), 
-                          "FTE-reale" = hworked/(36*45.6),
-                          "%tempo-utilizzato" = 100*(hworked/hprev),
-                          "tempo-medio-esame" = 60*(hworked/esami)) %>% 
-                   mutate(across(where(is.numeric), function(x) scale(x)))
-  )
-  
-  output$dipheat <- renderPlot(
-    dtx() %>% 
-      pivot_longer(2:9, names_to = "indicatore", values_to ="valore") %>% 
-      mutate(indicatore = factor(indicatore, levels = c("esami", "ricavi","hprev","hworked", "%tempo-utilizzato",
-                                                        "FTE-previsto", "FTE-reale", "tempo-medio-esame"))) %>%
-      ggplot(aes(Dipartimento,indicatore, label = round(valore,1))) + 
-      geom_tile(aes(fill = valore), colour = "white") + geom_text(color = "black", size = 5)+
-      scale_fill_gradient(low = "snow2", high = "steelblue")+theme_grey(base_size = 18) +
-      coord_flip()+labs(x = "", y = "", title="") + theme(legend.position = "none") +
-      scale_x_discrete(expand = c(0, 0)) +
-      scale_y_discrete(expand = c(0, 0), position = "right")+
-      theme(axis.text.x = element_text(color="blue", size=10, face="bold"))+
-      theme(axis.text.y = element_text(color="blue", size=10, face="bold"))
-  )
-  
-  output$tdip <- renderTable(
-    dati %>% ungroup() %>%
-      group_by(Dipartimento) %>%
-      summarise(hworked = sum(hworked),
-                hprev = sum(hprev),
-                esami = sum(esami),
-                ricavi = sum (ricavi)) %>%
-      mutate("FTE-previsto" = hprev/(36*45.6),
-             "FTE-reale" = hworked/(36*45.6),
-             "%tempo-utilizzato" = 100*(hworked/hprev),
-             "tempo-medio-esame" = 60*(hworked/esami))
-  )
-    
-  ####Dipartimento/Reparti#####
-  
-  dt3 <-reactive(
-    dati %>% ungroup() %>%
-      filter(Dipartimento == input$dip) %>%
-      group_by(Reparto) %>%
-      summarise(hworked = sum(hworked)/1000,
-                hprev = sum(hprev)/1000,
-                esami = sum(esami)/1000,
-                ricavi = sum (ricavi)/1000) %>%
-      mutate("FTEpr" = (1000*hprev)/(36*45.6),
-             "FTEr" = (1000*hworked)/(36*45.6),
-             "Perchwd" = 100*(hworked/hprev),
-             "tempo medio esame" = 60*(hworked/esami)) %>%
-      pivot_longer(2:9, names_to = "indicatore", values_to ="valore") %>%
-      filter(indicatore==input$indicatore2))
-  
-  
-  output$drplot <- renderPlot({
-    
-    dt3() %>%
-      arrange(valore) %>%
-      mutate(Reparto = factor(Reparto, levels = .$Reparto)) %>%
-      mutate(mediana = median(valore, na.rm = TRUE)) %>%
-      ggplot(aes(x=Reparto, y=valore, label = round(valore, 1)))+
-      geom_point( aes(x=Reparto, y=valore),color="lightblue", size = 15 )+
-      geom_text(color = "black", size = 5)+
-      geom_segment(aes(y = mediana, x = Reparto, yend = valore, xend = Reparto ),color = "grey50") +
-      coord_flip() +
-      theme_ipsum_rc(axis_title_just = "mc")+
-      labs(y=input$indicatore2,x="")+
-      theme(axis.text.x = element_text(color="blue", size=12, face="bold"))+
-      theme(axis.text.y = element_text(color="blue", size=12, face="bold"))+
-      theme(axis.title.x = element_text(size = 12, face = "bold"))
+  output$ra <- renderValueBox({
+    valueBox(
+      tabella %>% 
+        filter(Dipartimento == "Total") %>% 
+        select("RA"), "Ricavi per attività analitica", icon = icon("keyboard"),
+      color = "yellow"
+    )
   })
   
   
-  dtx3 <-reactive(dati %>% ungroup() %>% 
-                    filter(Dipartimento == input$dip) %>%
-                    group_by(Reparto) %>% 
-                    summarise(hworked = sum(hworked), 
-                              hprev = sum(hprev), 
-                              esami = sum(esami), 
-                              ricavi = sum (ricavi)) %>% 
-                    mutate("FTE-previsto" = hprev/(36*45.6), 
-                           "FTE-reale" = hworked/(36*45.6),
-                           "%tempo-utilizzato" = 100*(hworked/hprev),
-                           "tempo-medio-esame" = 60*(hworked/esami)) %>% 
-                    mutate(across(where(is.numeric), function(x) scale(x)))
-  )
-  
-  output$drheat <- renderPlot(
-    dtx3() %>% 
-      pivot_longer(2:9, names_to = "indicatore", values_to ="valore") %>% 
-      mutate(indicatore = factor(indicatore, levels = c("esami", "ricavi","hprev","hworked", "%tempo-utilizzato",
-                                                        "FTE-previsto", "FTE-reale", "tempo-medio-esame"))) %>%
-      ggplot(aes(Reparto,indicatore, label = round(valore,1))) + 
-      geom_tile(aes(fill = valore), colour = "white") + geom_text(color = "black", size = 5)+
-      scale_fill_gradient(low = "snow2", high = "steelblue")+theme_grey(base_size = 18) +
-      coord_flip()+labs(x = "", y = "", title="") + theme(legend.position = "none") +
-      scale_x_discrete(expand = c(0, 0)) +
-      scale_y_discrete(expand = c(0, 0), position = "right")+
-      theme(axis.text.x = element_text(color="blue", size=10, face="bold"))+
-      theme(axis.text.y = element_text(color="blue", size=10, face="bold"))
-  )
-  
-  output$tdiprep <- renderTable(
-    dati %>% ungroup() %>%
-      filter(Dipartimento == input$dip) %>% 
-      group_by(Reparto) %>%
-      summarise(hworked = sum(hworked),
-                hprev = sum(hprev),
-                esami = sum(esami),
-                ricavi = sum (ricavi)) %>%
-      mutate("FTE-previsto" = hprev/(36*45.6),
-             "FTE-reale" = hworked/(36*45.6),
-             "%tempo-utilizzato" = 100*(hworked/hprev),
-             "tempo-medio-esame" = 60*(hworked/esami))
-  )
-  
-####Dipartimento/Laboratori
-
-  dt5 <-reactive(
-    dati %>% ungroup() %>%
-      filter(Dipartimento == input$dip2) %>%
-      group_by(Laboratorio) %>%
-      summarise(hworked = sum(hworked)/1000,
-                hprev = sum(hprev)/1000,
-                esami = sum(esami)/1000,
-                ricavi = sum (ricavi)/1000) %>%
-      mutate("FTEpr" = (1000*hprev)/(36*45.6),
-             "FTEr" = (1000*hworked)/(36*45.6),
-             "Perchwd" = 100*(hworked/hprev),
-             "tempo medio esame" = 60*(hworked/esami)) %>%
-      pivot_longer(2:9, names_to = "indicatore", values_to ="valore") %>%
-      filter(indicatore==input$indicatore3))
+  prettyNum(totes,big.mark = ","),  icon = "fa-flask"
   
   
   
-  output$dlplot <- renderPlot({
-    dt5() %>%
-      arrange(valore) %>%
-      mutate(Laboratorio = factor(Laboratorio, levels = .$Laboratorio)) %>%
-      mutate(media = mean(valore, na.rm = TRUE)) %>%
-      ggplot(aes(x=Laboratorio, y=valore, label = round(valore, 1)))+
-      geom_point( aes(x=Laboratorio, y=valore),color="lightblue", size = 15 )+
-      geom_text(color = "black", size = 5)+
-      geom_segment(aes(y = media, x = Laboratorio, yend = valore, xend = Laboratorio ),color = "grey50") +
-      coord_flip() +
-      theme_ipsum_rc(axis_title_just = "mc")+
-      labs(y=input$indicatore3,x="")+
-      theme(axis.text.x = element_text(color="blue", size=12, face="bold"))+
-      theme(axis.text.y = element_text(color="blue", size=12, face="bold"))+
-      theme(axis.title.x = element_text(size = 12, face = "bold"))
-  })
   
   
-  dtx4 <-reactive(dati %>% ungroup() %>% 
-                    filter(Dipartimento == input$dip2) %>%
-                    group_by(Laboratorio) %>% 
-                    summarise(hworked = sum(hworked)/1000, 
-                              hprev = sum(hprev)/1000, 
-                              esami = sum(esami), 
-                              ricavi = sum (ricavi)) %>% 
-                    mutate("FTE-previsto" = (1000*hprev)/(36*45.6), 
-                           "FTE-reale" = (1000*hworked)/(36*45.6),
-                           "%tempo-utilizzato" = 100*(hworked/hprev),
-                           "tempo-medio-esame" = 60*(hworked/esami)) %>% 
-                    mutate(across(where(is.numeric), function(x) scale(x)))
-  )
   
-  output$dlheat <- renderPlot(
-    dtx4() %>% 
-      pivot_longer(2:9, names_to = "indicatore", values_to ="valore") %>% 
-      mutate(indicatore = factor(indicatore, levels = c("esami", "ricavi","hprev","hworked", "%tempo-utilizzato",
-                                                        "FTE-previsto", "FTE-reale", "tempo-medio-esame"))) %>%
-      ggplot(aes(Laboratorio,indicatore, label = round(valore,1))) + 
-      geom_tile(aes(fill = valore), colour = "white") + geom_text(color = "black", size = 5)+
-      scale_fill_gradient(low = "snow2", high = "steelblue")+theme_grey(base_size = 18) +
-      coord_flip()+labs(x = "", y = "", title="") + theme(legend.position = "none") +
-      scale_x_discrete(expand = c(0, 0)) +
-      scale_y_discrete(expand = c(0, 0), position = "right")+
-      theme(axis.text.x = element_text(color="blue", size=10, face="bold"))+
-      theme(axis.text.y = element_text(color="blue", size=10, face="bold"))
-  )
   
-  output$tdiplab <- renderTable(
-    dati %>% ungroup() %>%
-      filter(Dipartimento == input$dip2) %>% 
-      group_by(Laboratorio) %>%
-      summarise(hworked = sum(hworked),
-                hprev = sum(hprev),
-                esami = sum(esami),
-                ricavi = sum (ricavi)) %>%
-      mutate("FTE-previsto" = hprev/(36*45.6),
-             "FTE-reale" = hworked/(36*45.6),
-             "%tempo-utilizzato" = 100*(hworked/hprev),
-             "tempo-medio-esame" = 60*(hworked/esami))
-  )
-
-  #### DATASET####
- 
-  output$dataset <- DT::renderDataTable(dati)
- 
-
-###PivotTable####
-
-output$test<- rpivotTable::renderRpivotTable({
-  rpivotTable(dati)
-})
-
-}
+  }
