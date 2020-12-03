6a53d836474c4a02c09b01880976c96d20390150
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2020-12-03 15:53:59 +0100

    update

--- PROGRAMMAZIONE/scripts/COF.R
+++ PROGRAMMAZIONE/scripts/COF.R
@@ -10,4 +10,15 @@ library("flextable")
 library("shinyBS")
 library("officer")
 
+pr <- read_excel(here("programmazione", "data", "raw", "DatiProgettiUO.xlsx"))
+
+
+pr %>% 
+  mutate("Stato" = ifelse(DataFine < as.Date("2019-01-01"), "Archiviato", "Aperto")) %>% 
+  filter(Stato == "Aperto" & DataInizio <= as.Date("2019-12-31")) %>% 
+  mutate("Statoanno" = ifelse(DataFine <=as.Date("2019-12-31"), "Concluso", "Attivo")) %>% View()
+
+
+
+
  
