9bef692a7a82555bd5f9e17251a3d48a72e79538
Massimo Tranquillo <tmax@Air-di-Massimo.lan>
2020-12-03 19:05:32 +0100

    update

--- PROGRAMMAZIONE/scripts/COF.R
+++ PROGRAMMAZIONE/scripts/COF.R
@@ -11,12 +11,26 @@ library("shinyBS")
 library("officer")
 
 pr <- read_excel(here("programmazione", "data", "raw", "DatiProgettiUO.xlsx"))
+repMat <- readRDS( here("programmazione", "data", "processed", "matrperpubb.rds")) # carico i dati delle matricole per dip/rep/lab vedi preparazione dati.R in script
 
 
-pr %>% 
+
+pr %>% select(-14, -15) %>% 
   mutate("Stato" = ifelse(DataFine < as.Date("2019-01-01"), "Archiviato", "Aperto")) %>% 
   filter(Stato == "Aperto" & DataInizio <= as.Date("2019-12-31")) %>% 
   mutate("Statoanno" = ifelse(DataFine <=as.Date("2019-12-31"), "Concluso", "Attivo")) %>% View()
+  
+
+
+
+left_join(repMat, by = c("MatrRSUO" = "matricola")) %>% View()
+
+  
+    
+group_by(Dipartimento, Reparto, NumUoProgetto, Codice) %>% View()
+
+ 
+
 
 
 
