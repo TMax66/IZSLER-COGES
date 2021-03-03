ad8c3ff17e3f117c249806a3531455fadf5605e7
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2021-03-03 12:20:54 +0100

    update

--- PROGRAMMAZIONE/scripts/COF.R
+++ PROGRAMMAZIONE/scripts/COF.R
@@ -1,15 +1,14 @@
 library("tidyverse")
 library("readxl")
-library("RColorBrewer")
-library("shiny")
-library("shinydashboard")
+#library("RColorBrewer")
+# library("shiny")
+# library("shinydashboard")
 library("here")
-library("janitor")
-library("here")
+#library("janitor")
 library("flextable")
-library("shinyBS")
-library("officer")
-library("fmsb")
+# library("shinyBS")
+# library("officer")
+# library("fmsb")
 
 dati21 <- read_excel(here("programmazione", "data", "raw", "presenze2021.xlsx"))
 
@@ -115,9 +114,11 @@ writexl::write_xlsx(matricole, path= "matricole.xlsx")
 #####################################################################################################################
 
 
-obfted<- read_excel(here("programmazione", "data", "raw", "obiettiviXSB.xlsx"), sheet = "FTEDdipsan")
+obfted <- read_excel(here("programmazione", "data", "raw", "obiettiviXSB.xlsx"), sheet = "FTEDdipsan")
+obftec <- read_excel(here("programmazione", "data", "raw", "obiettiviXSB.xlsx"), sheet = "FTECdipsan")
 
-dt <- obfted %>% 
+dtD <- obfted %>% 
+  mutate(obcod = paste("OB", seq(1:nrow(.)))) %>% 
   pivot_longer(3:27, names_to = "struttura", values_to = "FTED") %>% 
   mutate(reparto = recode(struttura, "STBO" = "STBO-FE-MO", 
                                "STFE" = "STBO-FE-MO", 
@@ -156,11 +157,68 @@ dt <- obfted %>%
          )  
   
 
-dt %>% 
-  filter(dipartimento == "Dipartimento Tutela Salute Animale") %>% 
-  filter(!is.na(FTED)) %>% 
-  group_by(Obiettivo, dipartimento) %>% 
-  summarise(FTED = sum(FTED, na.rm=T)) %>% 
+
+
+
+
+dtC <- obftec %>% 
+  mutate(obcod = paste("OB", seq(1:nrow(.)))) %>% 
+  pivot_longer(3:27, names_to = "struttura", values_to = "FTEC") %>% 
+  mutate(reparto = recode(struttura, "STBO" = "STBO-FE-MO", 
+                          "STFE" = "STBO-FE-MO", 
+                          "STMO" = "STBO-FE-MO", 
+                          "STPR" = "STBO-PR-PC", 
+                          "STPC" = "STBO-PR-PC",
+                          "STFO" = "STBO-FO-RA", 
+                          "STRA" = "STBO-FO-RA", 
+                          "STBG" = "STBG-BI-SO", 
+                          "STBI" = "STBG-BI-SO", 
+                          "STSO" = "STBG-BI-SO", 
+                          "STLO" = "STBG-LO-MI", 
+                          "STMI" = "STBG-LO-MI", 
+                          "STCR" = "STBG-CR-MN", 
+                          "STMN" = "STBG-CR-MN"), 
+         
+         dipartimento = recode(reparto, "STBO-FE-MO" = "Area Territoriale Emilia Romagna", 
+                               "STBO-PR-PC" = "Area Territoriale Emilia Romagna", 
+                               "STBO-FO-RA" = "Area Territoriale Emilia Romagna", 
+                               "STBO-PR-PC" = "Area Territoriale Emilia Romagna", 
+                               "STRE" = "Area Territoriale Emilia Romagna", 
+                               "STBG-BI-SO" = "Area Territoriale Lombardia", 
+                               "STBG-LO-MI" = "Area Territoriale Lombardia", 
+                               "STBG-CR-MN" = "Area Territoriale Lombardia", 
+                               "STPV" = "Area Territoriale Lombardia", 
+                               "STBS" = "Area Territoriale Lombardia", 
+                               "RPP" = "Dipartimento Sicurezza Alimentare", 
+                               "RCABO" = "Dipartimento Sicurezza Alimentare", 
+                               "RCA" = "Dipartimento Sicurezza Alimentare", 
+                               "RCAM" = "Dipartimento Sicurezza Alimentare", 
+                               "RVIR" = "Dipartimento Tutela Salute Animale", 
+                               "RVVPB" = "Dipartimento Tutela Salute Animale", 
+                               "RTBA" =  "Dipartimento Tutela Salute Animale", 
+                               "RPCMB" = "Dipartimento Tutela Salute Animale"
+         )
+  )  
+
+
+
+
+
+
+
+
+
+
+dtD %>% 
+  right_join(dtC,  by = c( "dipartimento", "reparto", "struttura",   "obcod"))%>% 
   View()
+  
+  
+  
+  # filter(dipartimento == "Dipartimento Tutela Salute Animale") %>% 
+  # filter(!is.na(FTED)) %>% 
+  # group_by(Obiettivo, dipartimento) %>% 
+  # summarise(FTED = sum(FTED, na.rm=T)) %>% 
+  # View()
 
   
