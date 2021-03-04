40457a845902e19dc2b930fcd8f1726fed2849a7
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2021-03-04 16:03:38 +0100

    update

--- PROGRAMMAZIONE/shinyapp-in-sviluppo/ui.R
+++ PROGRAMMAZIONE/shinyapp-in-sviluppo/ui.R
@@ -11,7 +11,7 @@ ui <- dashboardPage(
       menuItem("Note", tabName = "help", icon = icon("book")), 
       hr(),
       br(), 
-      menuItem("Strumento di programmazione", tabName = "progr", icon = icon("calculator"))
+      menuItem("Programmazione 2021", tabName = "progr", icon = icon("calculator"))
     )
   ),
   dashboardBody(
@@ -279,53 +279,58 @@ tabItem(tabName = "help",
         ), 
 
 ##Programmazione#### 
-tabItem(tabName = "progr", 
-        
-        wellPanel(
-          fluidRow( 
-            column(3,  
-                   h3("Programmazione"),            
-                   numericInput("rt", "Ricavo Totale previsto",  value = "1000000"), 
-                   br(), 
-                   numericInput("fte", "Full Time Equivalenti disponibili ",  value = "100"), 
-                   br(), 
-                   # numericInput("ftet", "Full Time Equivalenti teorico",  value = ""), 
-                   # br(),
-                   sliderInput("pc", "percentuale FTE allocata agli obiettivi", min=0, max= 50,  value = "0")), 
-            
-            column(9, 
-                   valueBoxOutput("rfteT"),
-                   valueBoxOutput("ftep"), 
-                   valueBoxOutput("rfteP"), 
-                   valueBoxOutput("target")
-                  
-                   # tableOutput("tb")  
-                   
-                   
-                   
-            ))), 
-        br(),br(),br(),
-        
-        wellPanel(
-          fluidRow(
-            column(3, 
-                   h3("Verifica"),
-                   sliderInput("Vrt", "Variazione percentuale del Ricavo Totale previsto", min=-50, max= 50,  value = 0),
-                   br(), 
-                   sliderInput("Vfte", "Variazione percentuale del FTE programmato ", min=-50, max= 50,  value = 0)),
-            
-            column(9, 
-                   valueBoxOutput("rtot"),
-                   valueBoxOutput("fteR"), 
-                   valueBoxOutput("rfteR"),
-                   valueBoxOutput("target2")#, 
-                   #valueBoxOutput("risn")
-                   
-                   
-                   # tableOutput("tb2") 
-            )))
-        
-)
+# tabItem(tabName = "progr", 
+#         
+#         wellPanel(
+#           fluidRow( 
+#             column(3,  
+#                    h3("Programmazione"),            
+#                    numericInput("rt", "Ricavo Totale previsto",  value = "1000000"), 
+#                    br(), 
+#                    numericInput("fte", "Full Time Equivalenti disponibili ",  value = "100"), 
+#                    br(), 
+#                    # numericInput("ftet", "Full Time Equivalenti teorico",  value = ""), 
+#                    # br(),
+#                    sliderInput("pc", "percentuale FTE allocata agli obiettivi", min=0, max= 50,  value = "0")), 
+#             
+#             column(9, 
+#                    valueBoxOutput("rfteT"),
+#                    valueBoxOutput("ftep"), 
+#                    valueBoxOutput("rfteP"), 
+#                    valueBoxOutput("target")
+#                   
+#                    # tableOutput("tb")  
+#                    
+#                    
+#                    
+#             ))), 
+#         br(),br(),br(),
+#         
+#         wellPanel(
+#           fluidRow(
+#             column(3, 
+#                    h3("Verifica"),
+#                    sliderInput("Vrt", "Variazione percentuale del Ricavo Totale previsto", min=-50, max= 50,  value = 0),
+#                    br(), 
+#                    sliderInput("Vfte", "Variazione percentuale del FTE programmato ", min=-50, max= 50,  value = 0)),
+#             
+#             column(9, 
+#                    valueBoxOutput("rtot"),
+#                    valueBoxOutput("fteR"), 
+#                    valueBoxOutput("rfteR"),
+#                    valueBoxOutput("target2")#, 
+#                    #valueBoxOutput("risn")
+#                    
+#                    
+#                    # tableOutput("tb2") 
+#             )))
+#         
+# )
+
+tabItem(tabName = "progr") 
+
+
+
 
 
 
