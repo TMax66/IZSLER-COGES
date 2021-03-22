9df08f026e394ffd442a562b1fd4f3d47890fcab
Massimo Tranquillo <tmax@MacBook-Air-di-Massimo.local>
2021-03-22 15:47:32 +0100

    update

--- PROGRAMMAZIONE/piramideR/codici.R
+++ PROGRAMMAZIONE/piramideR/codici.R
@@ -14,46 +14,28 @@ library("lubridate")
 library("fmsb")
 
 ##PROGETTI DI RICERCA####
-
-anag21 <- readRDS(here("programmazione", "data", "processed", "ANAGRAFE2021.rds"))
 pr <- read_excel(here("programmazione", "piramideR", "pr2020.xlsx"))
 
-pr %>% 
+
+anag <- readRDS(here("programmazione", "data", "processed", "ANAGRAFE.rds"))
+
+pr <- pr %>% 
   #select(-14, -15) %>% 
-  mutate("Stato" = ifelse(DataFine < as.Date("2020-01-01"), "Archiviato", "Attivo")) %>% 
-  filter(Stato == "Attivo" & DataInizio <= as.Date("2020-12-31")) %>% 
-  mutate("Statoanno" = ifelse(DataFine <=as.Date("2020-12-31"), "Concluso", "Aperto")) %>% 
+  # mutate("Stato" = ifelse(DataFine < as.Date("2020-01-01"), "Archiviato", "Attivo")) %>% 
+  # filter(Stato == "Attivo" & DataInizio <= as.Date("2020-12-31")) %>% 
+  # mutate("Statoanno" = ifelse(DataFine <=as.Date("2020-12-31"), "Concluso", "Aperto")) %>% 
   mutate(MatrRSUO = ifelse(is.na(MatrRSUO), MatrRS, MatrRSUO)) %>% 
-  left_join(anag21, by = c("MatrRSUO" = "Matricola")) %>% View()
+  left_join(anag, by = c("MatrRSUO" = "Matricola"))
 
 
-
-#repMat <- readRDS( here("programmazione", "data", "processed", "matrperpubb.rds")) # carico i dati delle matricole per dip/rep/lab vedi preparazione dati.R in script
-
-## andare allo script di preparazione repMat ed eliminare la variabile Laboratorio che duplica le colonne dopo il join con pr
-
-
-repMat <- anag19 %>%
-  select("matricola" = CDMATR,
-         "sesso" = SESSO,
-         "dtnasc" = DTNASC,
-         "categoria" = DEMANSP3,
-         "hperc" = PCGIUR,
-         "contratto" = DECOMP) %>%
-  left_join(grusigma, by = "matricola") %>% 
-  mutate(matunique = !duplicated(matricola)) %>%
-  filter(matunique == "TRUE") %>%
-  right_join(hwd, by = "Matricola" ) %>% 
-  select(matricola,Dipartimento, Reparto )
 
 ###calcola per dipartimento/reparto/tipologia e codiceprg il numero di u.o. partecipanti e il budget
 
-pr %>% select(-14, -15) %>% 
-  mutate("Stato" = ifelse(DataFine < as.Date("2019-01-01"), "Archiviato", "Attivo")) %>% 
-  filter(Stato == "Attivo" & DataInizio <= as.Date("2019-12-31")) %>% 
-  mutate("Statoanno" = ifelse(DataFine <=as.Date("2019-12-31"), "Concluso", "Aperto")) %>%
-  left_join(repMat, by = c("MatrRSUO" = "matricola")) %>% View()
-filter(Statoanno == "Aperto") %>% 
+pr %>%
+  mutate("Stato" = ifelse(DataFine < as.Date("2000-01-01"), "Archiviato", "Attivo")) %>% 
+  filter(Stato == "Attivo" & DataInizio <= as.Date("2000-12-31")) %>% 
+  mutate("Statoanno" = ifelse(DataFine <=as.Date("2000-12-31"), "Concluso", "Aperto")) %>%
+filter(Statoanno == "Aperto") %>%  View()
   group_by(Dipartimento) %>% 
   summarise(Bdg = sum(Budget), 
             MBdg = mean(Budget, na.rm = T),
@@ -143,80 +125,103 @@ x = fct_infreq(Position)
        
        
        
-       ####codice per anagrafe 2021#####
-       # dati21 <- read_excel(here("programmazione", "data", "raw", "presenze2021.xlsx"))
-       # 
-       # 
-       # anag21 <- dati21 %>% 
-       #   mutate(REPARTO = recode(REPARTO, 
-       #                           "SEDE TERRITORIALE DI BERGAMO" = "SEDE TERRITORIALE DI BERGAMO - BINAGO - SONDRIO",
-       #                           "SEDE TERRITORIALE DI BINAGO" = "SEDE TERRITORIALE DI BERGAMO - BINAGO - SONDRIO", 
-       #                           "SEDE TERRITORIALE DI SONDRIO" = "SEDE TERRITORIALE DI BERGAMO - BINAGO - SONDRIO", 
-       #                           "SEDE TERRITORIALE DI CREMONA" = "SEDE TERRITORIALE DI CREMONA - MANTOVA",
-       #                           "SEDE TERRITORIALE DI MANTOVA" = "SEDE TERRITORIALE DI CREMONA - MANTOVA",
-       #                           "SEDE TERRITORIALE DI LODI" = "SEDE TERRITORIALE DI LODI - MILANO",
-       #                           "SEDE TERRITORIALE DI MILANO" = "SEDE TERRITORIALE DI LODI - MILANO",
-       #                           "SEDE TERRITORIALE DI BOLOGNA" = "SEDE TERRITORIALE DI BOLOGNA - MODENA - FERRARA",
-       #                           "SEDE TERRITORIALE DI MODENA" = "SEDE TERRITORIALE DI BOLOGNA - MODENA - FERRARA",
-       #                           "SEDE TERRITORIALE DI FERRARA" = "SEDE TERRITORIALE DI BOLOGNA - MODENA - FERRARA",
-       #                           "SEDE TERRITORIALE DI FORLÌ" = "SEDE TERRITORIALE DI FORLÌ - RAVENNA",
-       #                           "SEDE TERRITORIALE DI RAVENNA" = "SEDE TERRITORIALE DI FORLÌ - RAVENNA",
-       #                           "SEDE TERRITORIALE DI PIACENZA" = "SEDE TERRITORIALE DI PIACENZA - PARMA",
-       #                           "SEDE TERRITORIALE DI PARMA" = "SEDE TERRITORIALE DI PIACENZA - PARMA",
-       #                           "LABORATORIO CHIMICA APPLICATA ALLE TECNOLOGIE ALIMENTARI" = "REPARTO CHIMICA DEGLI ALIMENTI E MANGIMI", 
-       #                           "LABORATORIO BATTERIOLOGIA SPECIALIZZATA" = "REPARTO TECNOLOGIE BIOLOGICHE APPLICATE", 
-       #                           "LABORATORIO ANALISI GENOMICHE, LABORATORIO DIAGNOSTICA MOLECOLARE, OGM" = "REPARTO TECNOLOGIE BIOLOGICHE APPLICATE", 
-       #                           "LABORATORIO RESIDUI" = "REPARTO CHIMICA DEGLI ALIMENTI E MANGIMI", 
-       #                           "LABORATORIO MANGIMI E TOSSICOLOGIA" = "REPARTO CHIMICA DEGLI ALIMENTI E MANGIMI",
-       #                           "LABORATORIO DI CONTROLLO DI PRODOTTI BIOLOGICI, FARMACEUTICI E CONVALIDA DI PROCESSI PRODUTTIVI" = "REPARTO PRODUZIONE E CONTROLLO MATERIALE BIOLOGICO",
-       #                           "LABORATORIO PRODUZIONE TERRENI" = "REPARTO PRODUZIONE E CONTROLLO MATERIALE BIOLOGICO", 
-       #                           "LABORATORIO BENESSERE ANIMALE, BIOCHIMICA CLINICA, IMMUNOLOGIA VETERINARIA E STABULARI" = "REPARTO PRODUZIONE E CONTROLLO MATERIALE BIOLOGICO", 
-       #                           "LABORATORIO CONTAMINANTI AMBIENTALI" = "REPARTO CHIMICA DEGLI ALIMENTI E MANGIMI",
-       #                           "LABORATORIO DI VIROLOGIA E SIEROLOGIA SPECIALIZZATA, MICROSCOPIA ELETTRONICA" = "REPARTO VIROLOGIA",
-       #                           "LABORATORIO PRODUZIONE VACCINI E REAGENTI" = "REPARTO PRODUZIONE E CONTROLLO MATERIALE BIOLOGICO", 
-       #                           "LABORATORIO COLTURE CELLULARI, BIOBANCA" = "REPARTO TECNOLOGIE BIOLOGICHE APPLICATE"
-       #   ),
-       #   Dipartimento = recode (REPARTO, "REPARTO VIROLOGIA" = "Dipartimento Tutela e  Salute Animale", 
-       #                          "REPARTO TECNOLOGIE BIOLOGICHE APPLICATE" = "Dipartimento Tutela e  Salute Animale",
-       #                          "REPARTO PRODUZIONE E CONTROLLO MATERIALE BIOLOGICO" = "Dipartimento Tutela e  Salute Animale",
-       #                          "REPARTO VIRUS VESCICOLARI E PRODUZIONI BIOTECNOLOGICHE" = "Dipartimento Tutela e  Salute Animale", 
-       #                          "REPARTO PRODUZIONE PRIMARIA" = "Dipartimento Sicurezza Alimentare", 
-       #                          "REPARTO CONTROLLO ALIMENTI" = "Dipartimento Sicurezza Alimentare",
-       #                          "REPARTO CHIMICA DEGLI ALIMENTI E MANGIMI" = "Dipartimento Sicurezza Alimentare", 
-       #                          "REPARTO CHIMICO DEGLI ALIMENTI (BOLOGNA)" = "Dipartimento Sicurezza Alimentare", 
-       #                          "SEDE TERRITORIALE DI BERGAMO - BINAGO - SONDRIO" = "Area Territoriale Lombardia",
-       #                          "SEDE TERRITORIALE DI BRESCIA" = "Area Territoriale Lombardia",
-       #                          "SEDE TERRITORIALE DI PAVIA" = "Area Territoriale Lombardia",
-       #                          "SEDE TERRITORIALE DI CREMONA - MANTOVA" = "Area Territoriale Lombardia", 
-       #                          "SEDE TERRITORIALE DI LODI - MILANO" = "Area Territoriale Lombardia", 
-       #                          "SEDE TERRITORIALE DI FORLÌ - RAVENNA" = "Area Territoriale Emilia Romagna", 
-       #                          "SEDE TERRITORIALE DI BOLOGNA - MODENA - FERRARA" = "Area Territoriale Emilia Romagna", 
-       #                          "SEDE TERRITORIALE DI PIACENZA - PARMA" = "Area Territoriale Emilia Romagna", 
-       #                          "SEDE TERRITORIALE DI REGGIO EMILIA" = "Area Territoriale Emilia Romagna", 
-       #                          "ANALISI DEL RISCHIO ED EPIDEMIOLOGIA GENOMICA" = "Direzione Sanitaria", 
-       #                          "CONTROLLO DI GESTIONE" = "Direzione Generale", 
-       #                          "DIREZIONE AMMINISTRATIVA" = "Direzione Ammninistrativa", 
-       #                          "DIREZIONE GENERALE" = "Direzione Generale", 
-       #                          "DIREZIONE SANITARIA" = "Direzione Sanitaria",
-       #                          "FORMAZIONE E BIBLIOTECA" = "Direzione Sanitaria", 
-       #                          "GARE CONTRATTI PER ACQUISTO DI BENI E SERVIZI, MAGAZZINO E VENDITE, UFFICIO SERVIZI" = "Direzione Ammninistrativa",
-       #                          "GESTIONE CENTRALIZZATA DELLE RICHIESTE" = "Direzione Sanitaria", 
-       #                          "PROGETTAZIONE E DIREZIONE LAVORI MANUTENZIONI" = "Direzione Ammninistrativa", 
-       #                          "PROGETTI DI RICERCA" = "Direzione Generale", 
-       #                          "SERVIZIO ASSICURAZIONE QUALITA'" = "Direzione Generale",
-       #                          "SISTEMI INFORMATIVI" = "Direzione Generale",
-       #                          "SORVEGLIANZA EPIDEMIOLOGICA EMILIA ROMAGNA" = "Direzione Sanitaria", 
-       #                          "SORVEGLIANZA EPIDEMIOLOGICA LOMBARDIA" = "Direzione Sanitaria", 
-       #                          "U.O. AFFARI GENERALI E LEGALI" = "Direzione Ammninistrativa", 
-       #                          "U.O. GESTIONE RISORSE UMANE E SVILUPPO COMPETENZE" = "Direzione Ammninistrativa", 
-       #                          "U.O. GESTIONE SERVIZI CONTABILI" = "Direzione Ammninistrativa", 
-       #                          "LABORATORIO DI PROTEOMICA E DIAGNOSTICA TSE" = "Dipartimento Tutela e  Salute Animale"
-       #   ),
-       #   CENTRO_DI_COSTO = recode(CENTRO_DI_COSTO, "LABORATORIO DIAGNOSTICA GENERALE, SIEROLOGIA, BIOLOGIA MOLECOLARE E MICROBIOLOGIA" = "SEDE TERRITORIALE DI PIACENZA",
-       #                            "LABORATORIO LATTE" = "SEDE TERRITORIALE DI PIACENZA")
-       #   ) %>% 
-       #   select(Dipartimento, REPARTO, CENTRO_DI_COSTO, Dirigente, Matricola, Nome, Cognome, `Inizio Rapporto`, `Termine Rapporto`) 
-       #   #saveRDS(here("programmazione", "data", "processed", "ANAGRAFE2021.rds"))
-       
-       
+
+
+###codice per anagrafe#####
+anag <- read_excel(here("programmazione", "piramideR", "anagrafe.xlsx"))
+# 
+# 
+# anag %>%
+#   mutate(REPARTO = recode(dbo_IZS_Reparti.REPARTO,
+#                           "SEDE TERRITORIALE DI BERGAMO" = "SEDE TERRITORIALE DI BERGAMO - BINAGO - SONDRIO",
+#                           "Sezione di Bergamo" = "SEDE TERRITORIALE DI BERGAMO - BINAGO - SONDRIO", 
+#                           "SEDE TERRITORIALE DI BINAGO" = "SEDE TERRITORIALE DI BERGAMO - BINAGO - SONDRIO",
+#                           "Sezione di Binago" = "SEDE TERRITORIALE DI BERGAMO - BINAGO - SONDRIO",
+#                           "SEDE TERRITORIALE DI SONDRIO" = "SEDE TERRITORIALE DI BERGAMO - BINAGO - SONDRIO",
+#                           "SEDE TERRITORIALE DI CREMONA" = "SEDE TERRITORIALE DI CREMONA - MANTOVA",
+#                           "Sezione di Cremona" = "SEDE TERRITORIALE DI CREMONA - MANTOVA",
+#                           "SEDE TERRITORIALE DI MANTOVA" = "SEDE TERRITORIALE DI CREMONA - MANTOVA",
+#                           "Sezione di Mantova" = "SEDE TERRITORIALE DI CREMONA - MANTOVA",
+#                           "SEDE TERRITORIALE DI LODI" = "SEDE TERRITORIALE DI LODI - MILANO",
+#                           "Sezione di Lodi" = "SEDE TERRITORIALE DI LODI - MILANO",
+#                           "SEDE TERRITORIALE DI MILANO" = "SEDE TERRITORIALE DI LODI - MILANO",
+#                           "Sezione di Milano" = "SEDE TERRITORIALE DI LODI - MILANO",
+#                           "SEDE TERRITORIALE DI BOLOGNA" = "SEDE TERRITORIALE DI BOLOGNA - MODENA - FERRARA",
+#                           "Sezione di Bologna" = "SEDE TERRITORIALE DI BOLOGNA - MODENA - FERRARA",
+#                           "SEDE TERRITORIALE DI MODENA" = "SEDE TERRITORIALE DI BOLOGNA - MODENA - FERRARA",
+#                           "Sezione di Modena" = "SEDE TERRITORIALE DI BOLOGNA - MODENA - FERRARA",
+#                           "SEDE TERRITORIALE DI FERRARA" = "SEDE TERRITORIALE DI BOLOGNA - MODENA - FERRARA",
+#                           "Sezione di Ferrara" = "SEDE TERRITORIALE DI BOLOGNA - MODENA - FERRARA",
+#                           "SEDE TERRITORIALE DI FORLÌ" = "SEDE TERRITORIALE DI FORLÌ - RAVENNA",
+#                           "Sezione di Forlì" = "SEDE TERRITORIALE DI FORLÌ - RAVENNA",
+#                           "SEDE TERRITORIALE DI RAVENNA" = "SEDE TERRITORIALE DI FORLÌ - RAVENNA",
+#                           "Sezione di Ravenna" = "SEDE TERRITORIALE DI FORLÌ - RAVENNA", 
+#                           "SEDE TERRITORIALE DI PIACENZA" = "SEDE TERRITORIALE DI PIACENZA - PARMA",
+#                           "Sezione di Piacenza" = "SEDE TERRITORIALE DI PIACENZA - PARMA",
+#                           "SEDE TERRITORIALE DI PARMA" = "SEDE TERRITORIALE DI PIACENZA - PARMA",
+#                           "Sezione di Parma" = "SEDE TERRITORIALE DI PIACENZA - PARMA",
+#                           "Sezione di Brescia" = "SEDE TERRITORIALE DI BRESCIA", 
+#                           "Sezione di Reggio Emilia" = "SEDE TERRITORIALE DI REGGIO EMILIA", 
+#                           "Sezione di Pavia" =  "SEDE TERRITORIALE DI REGGIO EMILIA", 
+#                           "LABORATORIO CHIMICA APPLICATA ALLE TECNOLOGIE ALIMENTARI" = "REPARTO CHIMICA DEGLI ALIMENTI E MANGIMI",
+#                           "LABORATORIO BATTERIOLOGIA SPECIALIZZATA" = "REPARTO TECNOLOGIE BIOLOGICHE APPLICATE",
+#                           "LABORATORIO ANALISI GENOMICHE, LABORATORIO DIAGNOSTICA MOLECOLARE, OGM" = "REPARTO TECNOLOGIE BIOLOGICHE APPLICATE",
+#                           "LABORATORIO RESIDUI" = "REPARTO CHIMICA DEGLI ALIMENTI E MANGIMI",
+#                           "LABORATORIO MANGIMI E TOSSICOLOGIA" = "REPARTO CHIMICA DEGLI ALIMENTI E MANGIMI",
+#                           "LABORATORIO DI CONTROLLO DI PRODOTTI BIOLOGICI, FARMACEUTICI E CONVALIDA DI PROCESSI PRODUTTIVI" = "REPARTO PRODUZIONE E CONTROLLO MATERIALE BIOLOGICO",
+#                           "LABORATORIO PRODUZIONE TERRENI" = "REPARTO PRODUZIONE E CONTROLLO MATERIALE BIOLOGICO",
+#                           "LABORATORIO BENESSERE ANIMALE, BIOCHIMICA CLINICA, IMMUNOLOGIA VETERINARIA E STABULARI" = "REPARTO PRODUZIONE E CONTROLLO MATERIALE BIOLOGICO",
+#                           "LABORATORIO CONTAMINANTI AMBIENTALI" = "REPARTO CHIMICA DEGLI ALIMENTI E MANGIMI",
+#                           "LABORATORIO DI VIROLOGIA E SIEROLOGIA SPECIALIZZATA, MICROSCOPIA ELETTRONICA" = "REPARTO VIROLOGIA",
+#                           "LABORATORIO PRODUZIONE VACCINI E REAGENTI" = "REPARTO PRODUZIONE E CONTROLLO MATERIALE BIOLOGICO",
+#                           "LABORATORIO COLTURE CELLULARI, BIOBANCA" = "REPARTO TECNOLOGIE BIOLOGICHE APPLICATE",
+#                           
+#   ),
+#   Dipartimento = recode (REPARTO, "REPARTO VIROLOGIA" = "Dipartimento Tutela e  Salute Animale",
+#                          "REPARTO TECNOLOGIE BIOLOGICHE APPLICATE" = "Dipartimento Tutela e  Salute Animale",
+#                          "REPARTO PRODUZIONE E CONTROLLO MATERIALE BIOLOGICO" = "Dipartimento Tutela e  Salute Animale",
+#                          "REPARTO VIRUS VESCICOLARI E PRODUZIONI BIOTECNOLOGICHE" = "Dipartimento Tutela e  Salute Animale",
+#                          "REPARTO PRODUZIONE PRIMARIA" = "Dipartimento Sicurezza Alimentare",
+#                          "REPARTO CONTROLLO ALIMENTI" = "Dipartimento Sicurezza Alimentare",
+#                          "REPARTO CHIMICA DEGLI ALIMENTI E MANGIMI" = "Dipartimento Sicurezza Alimentare",
+#                          "REPARTO CHIMICO DEGLI ALIMENTI (BOLOGNA)" = "Dipartimento Sicurezza Alimentare",
+#                          "SEDE TERRITORIALE DI BERGAMO - BINAGO - SONDRIO" = "Area Territoriale Lombardia",
+#                          "SEDE TERRITORIALE DI BRESCIA" = "Area Territoriale Lombardia",
+#                          "SEDE TERRITORIALE DI PAVIA" = "Area Territoriale Lombardia",
+#                          "SEDE TERRITORIALE DI CREMONA - MANTOVA" = "Area Territoriale Lombardia",
+#                          "SEDE TERRITORIALE DI LODI - MILANO" = "Area Territoriale Lombardia",
+#                          "SEDE TERRITORIALE DI FORLÌ - RAVENNA" = "Area Territoriale Emilia Romagna",
+#                          "SEDE TERRITORIALE DI BOLOGNA - MODENA - FERRARA" = "Area Territoriale Emilia Romagna",
+#                          "SEDE TERRITORIALE DI PIACENZA - PARMA" = "Area Territoriale Emilia Romagna",
+#                          "SEDE TERRITORIALE DI REGGIO EMILIA" = "Area Territoriale Emilia Romagna",
+#                          "ANALISI DEL RISCHIO ED EPIDEMIOLOGIA GENOMICA" = "Direzione Sanitaria",
+#                          "CONTROLLO DI GESTIONE" = "Direzione Generale",
+#                          "DIREZIONE AMMINISTRATIVA" = "Direzione Ammninistrativa",
+#                          "DIREZIONE GENERALE" = "Direzione Generale",
+#                          "DIREZIONE SANITARIA" = "Direzione Sanitaria",
+#                          "Direttore Sanitario" = "Direzione Sanitaria", 
+#                          "FORMAZIONE E BIBLIOTECA" = "Direzione Sanitaria",
+#                          "GARE CONTRATTI PER ACQUISTO DI BENI E SERVIZI, MAGAZZINO E VENDITE, UFFICIO SERVIZI" = "Direzione Ammninistrativa",
+#                          "GESTIONE CENTRALIZZATA DELLE RICHIESTE" = "Direzione Sanitaria",
+#                          "PROGETTAZIONE E DIREZIONE LAVORI MANUTENZIONI" = "Direzione Ammninistrativa",
+#                          "PROGETTI DI RICERCA" = "Direzione Generale",
+#                          "SERVIZIO ASSICURAZIONE QUALITA'" = "Direzione Generale",
+#                          "SISTEMI INFORMATIVI" = "Direzione Generale",
+#                          "SORVEGLIANZA EPIDEMIOLOGICA EMILIA ROMAGNA" = "Direzione Sanitaria",
+#                          "SORVEGLIANZA EPIDEMIOLOGICA LOMBARDIA" = "Direzione Sanitaria",
+#                          "U.O. AFFARI GENERALI E LEGALI" = "Direzione Ammninistrativa",
+#                          "U.O. GESTIONE RISORSE UMANE E SVILUPPO COMPETENZE" = "Direzione Ammninistrativa",
+#                          "U.O. GESTIONE SERVIZI CONTABILI" = "Direzione Ammninistrativa",
+#                          "LABORATORIO DI PROTEOMICA E DIAGNOSTICA TSE" = "Dipartimento Tutela e  Salute Animale"
+#   )
+#   
+#   ) %>%
+#   select(Dipartimento, REPARTO, CENTRO_DI_COSTO, Dirigente,
+#          Matricola, Nome, Cognome, InizioRapporto, FineRapporto) %>% 
+# saveRDS(here("programmazione", "data", "processed", "ANAGRAFE.rds"))
+# 
+# 
+# 
+# 
+
        
