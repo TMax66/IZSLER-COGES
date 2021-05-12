78f9c1f454394e561f8c27cd6244f8d30ffab0b3
TMax66 <massimo.tranquillo@gmail.com>
2021-05-12 16:02:09 +0200

    update

--- PROGRAMMAZIONE/performances/connesione dbIZSLER.R
+++ PROGRAMMAZIONE/performances/connesione dbIZSLER.R
@@ -1,20 +1,51 @@
 library("DBI")
 library("odbc")
+library("tidyverse")
 
+##connessione al db####
 con <- DBI::dbConnect(odbc::odbc(),
                       
                       Driver   = "SQL Server",
                       
-                      Server   =  "dbprod02.izsler.it",
+                      Server   =  "CED-IIS2.izsler.it",
                       
-                      Database = "IZSLER",
+                      Database = "ObiettiviStrategiciV2018",
                       
                       Port = 1433)
 
 
-library("tidyverse")
-library("dbplyr")
+# query <- c("SELECT Descrizione FROM dbo.ObiettivoStrategico")
+# 
+# os <- tbl(con, sql("SELECT Prospettiva_ProspettivaId, Descrizione FROM dbo.ObiettivoStrategico"))
+# prosp <- tbl(con, sql("SELECT * FROM dbo.Prospettiva"))
+# os %>% 
+#    left_join(prosp, by= c("Prospettiva_ProspettivaId" = "ProspettivaId")) %>% data.frame() %>%  View()
+# openT <- function( db, tabella, ...)
+#           {
 
-x <- tbl(con, "Anag_Comuni")
 
-show_query(x)
+####codice per bypassare bug di odbc####
+column.types <- dbGetQuery(con, "SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+                           FROM INFORMATION_SCHEMA.COLUMNS 
+                           WHERE TABLE_NAME=  'SchedaValutazione' " )
+ct <- column.types %>%
+  mutate(cml = case_when(
+    is.na(CHARACTER_MAXIMUM_LENGTH) ~ 10,
+    CHARACTER_MAXIMUM_LENGTH == -1 ~ 100000,
+    TRUE ~ as.double(CHARACTER_MAXIMUM_LENGTH)
+  )
+  ) %>%
+  arrange(cml) %>%
+  pull(COLUMN_NAME)
+fields <- paste(ct, collapse=", ")
+query <- paste("SELECT", fields, paste("FROM", "SchedaValutazione"))
+
+# return(query)
+# }
+
+#query <- openT(db = con, tabella = Audit)
+
+#estraggo la tabella SchedaValutazione##
+SV <- tbl(con, sql(query)) %>% 
+  as_tibble()
+names(SV)
