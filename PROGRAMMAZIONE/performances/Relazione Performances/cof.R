a52d2e8afc222587db14682f97a314c1e620cf8c
TMax66 <massimo.tranquillo@gmail.com>
2021-05-28 16:10:13 +0200

    update

--- PROGRAMMAZIONE/performances/Relazione Performances/cof.R
+++ PROGRAMMAZIONE/performances/Relazione Performances/cof.R
@@ -47,7 +47,110 @@ d <- tibble("Ruolo" = c(rep("Dirigenza", 2), rep("Comparto", 2), rep("Borse di s
      axis.text.y=element_text(size=16),
      strip.text.x=element_text(size=16),
    )
-   
-   
-   
+
  
+ prj20 <- read_excel("PROGRAMMAZIONE/performances/Relazione Performances/2020 relazione performance tab e figure/progetti2020.xlsx")
+
+ 
+ prj20 %>% 
+    pivot_longer(names_to = "Stato", cols = 2:4) %>% 
+    mutate(Stato = factor(Stato, levels = c("In corso", "Conclusi", "Nuovi"))) %>% 
+    ggplot(aes(x= reorder(Tipologia, value),  y=value))+
+    geom_bar(stat = "identity", fill = "gray")+
+    geom_text(aes(label = value), position = "dodge")+
+    coord_flip()+
+    labs(
+       x = "",
+       y = "",
+       subtitle = "Distribuzione del numero dei progetti di ricerca nell'anno 2020"
+    ) +
+    facet_wrap( ~Stato)+
+    theme_ipsum_rc() 
+    theme(
+       axis.text.x=element_blank(),
+    )
+    
+
+form <- tibble("Tipologia" = c("Formazione sul campo", 
+                               "Residenziale", 
+                               "Formazione a distanza"), 
+               "N.corsi" = c(9, 31, 23))
+ 
+
+form %>% 
+   ggplot(aes(x= reorder(Tipologia, N.corsi ), y= N.corsi))+
+   geom_bar(stat = "identity", fill = "gray")+
+   geom_text(aes(label = N.corsi), position = "dodge")+
+   coord_flip()+
+   labs(
+      x = "",
+      y = "",
+      subtitle = "Distribuzione del numero di corsi erogati nel 2020"
+   ) +
+   theme_ipsum_rc() 
+theme(
+   axis.text.x=element_blank(),
+)
+
+library("here")
+
+ricercatori <- read_excel(here("programmazione",  "piramideR", "ricercatori.xlsx"))
+ricercatori <- ricercatori[-c(1259:1268),]
+ 
+
+ricercatori$Cognome <- gsub(",.*$", "", ricercatori$Name)
+ricercatori <- ricercatori %>%
+   mutate(Cognome = recode(Cognome, "Moreno" = "Moreno Martin", 
+                           "Martin" = "Moreno Martin", 
+                           "Elisabetta" = "Caprai", 
+                           "Cosciani-Cunico" = "Cosciani Cunico", 
+   ))
+
+
+ricercatori %>% 
+   filter(`Publication Year` == 2020) %>% 
+   group_by(Cognome) %>% 
+      summarise("Pubblicazioni" = sum(`Web of Science Documents`), 
+                "Citazioni" = sum(`Times Cited`), 
+                "Cit.Media" = median(Citazioni/Pubblicazioni)) %>% 
+   pivot_longer(names_to = "parametro", cols = 2:4) %>% 
+   mutate(parametro = factor(parametro, levels = c("Pubblicazioni", "Citazioni", "Cit.Media"))) %>% 
+   ggplot(aes(x = value))+
+   geom_histogram(bins = 15, fill = "gray", col = "black")+
+   facet_wrap(~parametro)+
+   labs(
+      x = "",
+      y = "",
+      subtitle = "Dati produttivi e citazionali dell'attività di pubblicazione scientifica dell'IZSLER nel 2020"
+   ) +
+   theme_ipsum_rc() 
+theme(
+   axis.text.x=element_blank(),
+)
+
+
+ricercatori %>% 
+   filter(`Publication Year` == 2020) %>% 
+   unique() %>% 
+   summarise(n=n())
+   
+ricercatori %>% 
+   filter(`Publication Year` == 2020) %>% 
+   group_by(Cognome) %>% 
+   summarise("Pubblicazioni" = sum(`Web of Science Documents`), 
+             "Citazioni" = sum(`Times Cited`), 
+             "Cit.Media" = mean(Citazioni/Pubblicazioni)) %>% 
+   ungroup() %>% 
+   summarise(mP= mean(Pubblicazioni), 
+             MP = median(Pubblicazioni),
+             minP = min(Pubblicazioni), 
+             maxP = max(Pubblicazioni), 
+             mCit = mean(Citazioni),
+             minCit = min(Citazioni), 
+             maxCit = max(Citazioni), 
+             MCit = median(Citazioni), 
+             mCitm = mean(Cit.Media), 
+             MCitm = median(Cit.Media), 
+             minMcitm = min(Cit.Media), 
+             maxMvitm = max(Cit.Media)) %>% View()
+   
