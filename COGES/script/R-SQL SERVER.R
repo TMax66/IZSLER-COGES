eee4849fd73e6437fb6286dd5bf90ca44f028a48
TMax66 <massimo.tranquillo@gmail.com>
2021-06-03 14:43:17 +0200

    update

--- COGES/script/R-SQL SERVER.R
+++ COGES/script/R-SQL SERVER.R
@@ -9,35 +9,35 @@ library(writexl)
 con <- DBI::dbConnect(odbc::odbc(), Driver = "SQL Server", Server = "dbtest02", 
                       Database = "DW_COGE_DEV", Port = 1433)
 DBI::dbListTables(con)### elenca tutte le tabelle del database
-con %>% tbl("nome tabella") ## estrae il contentuo di una tabella del db
+#con %>% tbl("nome tabella") ## estrae il contentuo di una tabella del db
 
+
+####tabella ore lavorate###------------------------------------------------------
 query <- "SELECT
-  dbo.Personale_V2020.Matricola,
   dbo.Personale_V2020.Anno,
+  dbo.Personale_V2020.Mese,
+  dbo.Personale_V2020.Matricola,
   dbo.IZS_Dipartimenti.DIPARTIMENTO,
   dbo.IZS_Reparti.REPARTO,
   dbo.IZS_CDC.CENTRO_DI_COSTO,
   dbo.Personale_V2020.Percentuale,
-  dbo.Personale_V2020.Ore,
   dbo.Personale_V2020.InizioRapporto,
   dbo.Personale_V2020.FineRapporto,
   dbo.Personale_V2020.Dirigente,
   dbo.Personale_V2020.Contratto,
-  dbo.Personale_V2020.Tempo,
-  dbo.Personale_V2020.Nome,
-  dbo.Personale_V2020.Cognome
+  dbo.Personale_V2020.Ore,
+  dbo.Personale_V2020.SmartWorking
 FROM
   dbo.Personale_V2020 INNER JOIN dbo.IZS_CDC ON (dbo.Personale_V2020.CDC=dbo.IZS_CDC.CODICE_CDC)
    INNER JOIN dbo.IZS_Reparti ON (dbo.IZS_CDC.CODICE_REPARTO=dbo.IZS_Reparti.CODICE_REPARTO)
    INNER JOIN dbo.IZS_Dipartimenti ON (dbo.IZS_Reparti.CODICE_DIPARTIMENTO=dbo.IZS_Dipartimenti.CODICE_DIPARTIMENTO)
   
 WHERE
-  dbo.Personale_V2020.Anno  =  2020
+  dbo.Personale_V2020.Anno  >=  2019
+
 "
-
-
-ore20 <- con %>% tbl(sql(query)) %>% as_tibble()
-View(ore20)
+ore19_21 <- con %>% tbl(sql(query)) %>% as_tibble()
+View(ore19_21)
 
 
 ## tabella conversione strutture
@@ -48,21 +48,11 @@ View(ore20)
 
 
 
-query2 <- "SELECT
-  dbo.IZS_ANNI.ANNO,
-  dbo.IZS_Reparti.CODICE_REPARTO,
-  dbo.IZS_Reparti.REPARTO
-FROM
-  dbo.IZS_Reparti INNER JOIN dbo.IZS_CDC ON (dbo.IZS_Reparti.CODICE_REPARTO=dbo.IZS_CDC.CODICE_REPARTO)
-   INNER JOIN dbo.CDC_MOVIMENTI_BO ON (dbo.IZS_CDC.CODICE_CDC=dbo.CDC_MOVIMENTI_BO.CDC)
-   INNER JOIN dbo.IZS_ANNI ON (dbo.IZS_ANNI.ANNO=dbo.CDC_MOVIMENTI_BO.ANNO)
-"
 
-coge20 <- con %>% tbl(sql(query2)) %>% as_tibble()
+
+coge20 <- con %>% tbl(sql(query)) %>% as_tibble()
 
 View(coge20)
 
 
-con %>% tbl(sql(query2))
 
-query2
